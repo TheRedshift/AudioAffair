7503f77917836e950cbb0521b22281b01955e680
Ben Thompson <benjfthompson@gmail.com>
2016-11-26 13:51:47 +0000

    fixed return type

--- src/processing.pyi
+++ src/processing.pyi
@@ -4,7 +4,7 @@ Date: 2016-11-26
 """
 from typing import *
 
-def get_output(wave: List[float]) -> List[List[float]]:
+def get_output(wave: List[float]) -> List[List[bool]]:
     """
     :param wave: the sliding waveform to be processed. Pass only the slice to be used for current diode illumination
     :return: a 2D array of booleans corresponding to the on/off state of the LED on the grid
