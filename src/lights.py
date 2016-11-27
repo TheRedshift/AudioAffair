6ec0fe2a5ffaf3cdf138537015474150ab504dba
TheRedshift <rsoni28124@gmail.com>
2016-11-27 04:12:03 +0000

    ready for testing on full grid

--- src/lights.py
+++ src/lights.py
@@ -2,26 +2,22 @@ __author__ = 'david'
 import RPi.GPIO as GPIO
 import time
 
-def updateLEDs(grid, x, y, sleepVal, period):
+
+def updateLEDs(grid, x, y):
     x_channel_list = x
     y_channel_list = y
     numx = numy = 0
     for x in grid:
         for y in x:
-            GPIO.output(x_channel_list[numx], GPIO.LOW)
             if (y):
-                GPIO.output(y_channel_list[numy], GPIO.HIGH)
+                GPIO.output(x_channel_list[numx], GPIO.HIGH)
                 GPIO.output(y_channel_list[numy], GPIO.LOW)
-            if(sleepVal):
-                time.sleep(float(period))
+                time.sleep(0.00125)
             GPIO.output(y_channel_list[numy], GPIO.HIGH)
             GPIO.output(x_channel_list[numx], GPIO.LOW)
             numy += 1
         numy =0
         numx +=1
-
-def flashLEDs(grid, x, y, period):
-    updateLEDs(grid, x, y, TRUE, period)
 
 def setupLEDs(x, y):
     x_channel_list = x
